d60596ddcd909593e441cd7d97c04b24bd3737c6
Natalie Ayuba <natalieayuba@hotmail.com>
2024-09-01 19:02:04 +0100

    Formatted app ad for desktop

--- src/app/components/home/AppAd.tsx
+++ src/app/components/home/AppAd.tsx
@@ -17,32 +17,38 @@ const AppAd = () => {
   ];
 
   return (
-    <div className='bg-lilac flex flex-col h-[480px] pt-14 px-8 gap-6 text-night text-center relative overflow-clip'>
-      <h2 className='font-medium text-xl leading-6'>
-        Get tailored recommendations with the Headfirst mobile app
-      </h2>
-      <div className='flex justify-center gap-3'>
-        {badges.map(({ image, link, alt }) => (
-          <Link key={alt} href={link}>
-            <Image
-              src={image}
-              alt={alt}
-              width={0}
-              height={0}
-              sizes='100vw'
-              className='h-10 w-auto'
-            />
-          </Link>
-        ))}
+    <div className='bg-lilac pt-16 px-8 text-night text-center overflow-hidden'>
+      <div className='content-container h-[480px] flex md:gap-10 flex-col md:flex-row md:justify-between'>
+        <div className='flex-1 md:h-full md:-mt-10 flex flex-col justify-center'>
+          <h2 className='font-medium mb-4 text-xl md:text-3xl leading-6'>
+            Get tailored recommendations with the Headfirst mobile app
+          </h2>
+          <div className='flex justify-center gap-3'>
+            {badges.map(({ image, link, alt }) => (
+              <Link key={alt} href={link}>
+                <Image
+                  src={image}
+                  alt={alt}
+                  width={0}
+                  height={0}
+                  sizes='100vw'
+                  className='h-10 w-auto'
+                />
+              </Link>
+            ))}
+          </div>
+        </div>
+        <div>
+          <Image
+            src='/headfirst-app-mock.png'
+            alt='Headfirst Bristol app'
+            width={0}
+            height={0}
+            sizes='100vw'
+            className='w-96 h-auto relative top-12 left-1/2 -translate-x-[45%]'
+          />
+        </div>
       </div>
-      <Image
-        src='/headfirst-app-mock.png'
-        alt='Headfirst Bristol app'
-        width={324}
-        height={566}
-        sizes='100vw'
-        className='absolute left-[52px] top-[200px]'
-      />
     </div>
   );
 };
